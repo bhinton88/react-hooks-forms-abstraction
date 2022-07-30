53f10ae5402759a64a3f0e70768a078c6ce3cd8c
Bobby Hinton <bob.g.hint@gmail.com>
2022-07-30 15:00:52 -0500

    commit tree 82e7f36545d345a9a6c9756aa8993f1912722b57

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,21 +1,37 @@
 import React, { useState } from "react";
 
 function Form() {
-  const [firstName, setFirstName] = useState("John");
-  const [lastName, setLastName] = useState("Henry");
 
-  function handleFirstNameChange(event) {
-    setFirstName(event.target.value);
-  }
+// abstracted away the need for two state functions into only one   
+const [formData, setFormData] = useState({
+    firstName: "John",
+    lastName: "Henry"
+  })
 
-  function handleLastNameChange(event) {
-    setLastName(event.target.value);
+  function handleChange(event) {
+    const name = event.target.name // we set name attributes below so that when the input is submitted we can target that specific name 
+    const value = event.target.value
+
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
   }
 
   return (
     <form>
-      <input type="text" onChange={handleFirstNameChange} value={firstName} />
-      <input type="text" onChange={handleLastNameChange} value={lastName} />
+      <input 
+        type="text" 
+        name="firstName" // giving our form input a name attribute that matches the key in our object will allow us to abstract away two diff naming functions 
+        onChange={handleChange} 
+        value={formData.firstName} 
+      />
+      <input 
+        type="text" 
+        name="lastName"
+        onChange={handleChange} 
+        value={formData.lastName} 
+      />
       <button type="submit">Submit</button>
     </form>
   );
